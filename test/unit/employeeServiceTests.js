2679003942cb90878e757167c4462125841d1fd7
OlegKleyman <o.kleyman@us.nature.com>
2018-03-26 12:17:04 -0400

    add sinon to service tests

--- test/unit/employeeServiceTests.js
+++ test/unit/employeeServiceTests.js
@@ -2,26 +2,23 @@
 
 var expect = require('chai').expect;
 var service = require('../../lib/employeeService');
+var sinon = require('sinon');
 
 describe('employeeService tests', function() {
-    var getDataStore = seedData => (
+    var getDataStore = () => (
         {
-            getAll: () => seedData,
-            getSingle: filter => seedData.find(filter),
-            add: function(employee){
-                var maxId = seedData.map(emp => emp.id).reduce((previous, current) => Math.max(current, previous), 1)
-                employee.id = maxId + 1;
-                seedData.push(employee)
-    
-                return employee.id;
-            }
+            getAll: () => {},
+            getSingle: filter => {},
+            add: employee => {}
         });
 
     var getEmployeeService = dataStore => service(dataStore);;
 
     it('getAll should return employees from the data store', function(done) {
         var testData = [{name: 'test'}, {name: 'test2'}];
-        var employeeService = getEmployeeService(getDataStore(testData));
+        var store = getDataStore();
+        sinon.stub(store, 'getAll').returns(testData);
+        var employeeService = getEmployeeService(store);
 
         expect(employeeService.getAll()).to.equal(testData);
 
@@ -29,8 +26,10 @@ describe('employeeService tests', function() {
     });
 
     it('getById should return employee from the data store with the target ID', function(done) {
-        var testData = [{name: 'test', id: 1}, {name: 'test2', id: 2}];
-        var employeeService = getEmployeeService(getDataStore(testData));
+        var testData = {name: 'test2', id: 2};
+        var store = getDataStore();
+        sinon.stub(store, 'getSingle').withArgs(2).returns(testData);
+        var employeeService = getEmployeeService(store);
 
         expect(employeeService.getById(2)).to.equal(testData[1]);
 
@@ -38,8 +37,9 @@ describe('employeeService tests', function() {
     });
 
     it('add should return the ID of added employee', function(done) {
-        var testData = [{name: 'test', id: 1}, {name: 'test2', id: 2}];
-        var employeeService = getEmployeeService(getDataStore(testData));
+        var store = getDataStore();
+        sinon.stub(store, 'add').withArgs({name: 'test3'}).returns(3);
+        var employeeService = getEmployeeService(store);
 
         expect(employeeService.add({name: 'test3'})).to.equal(3);
 
