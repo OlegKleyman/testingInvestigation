8f7f2139e62c9078c74f1c8e932afa03bda83e94
OlegKleyman <o.kleyman@us.nature.com>
2018-04-02 12:06:56 -0400

    remove redundant code and change variable name to clearly identify its purpose

--- test/unit/employeeDataStoreTests.js
+++ test/unit/employeeDataStoreTests.js
@@ -5,12 +5,7 @@ var dataStore = require('../../lib/employeeDataStore');
 var sinon = require('sinon');
 
 describe('employeeDataStore tests', function() {
-    var getDatabase = seedData => ({
-            getData: (target) => seedData,
-            push: (path, target, overwrite) => seedData.push(target)
-        });
-
-    var getDatabase1 = () => ({
+    var getDatabase = () => ({
         getData: target => {},
         push: (path, target, overwrite) => {}
     });
@@ -19,7 +14,7 @@ describe('employeeDataStore tests', function() {
 
     it('getAll should return employees from the underline data store', function(done) {
         var testData = [{name: 'test'}, {name: 'test2'}];
-        var database = getDatabase1();
+        var database = getDatabase();
         sinon.stub(database, 'getData').withArgs('/employees').returns(testData);
         var dataStore = getEmployeeDataStore(database);
 
@@ -30,7 +25,7 @@ describe('employeeDataStore tests', function() {
 
     it('getSingle should return employee from the underline data store by filter argument', function(done) {
         var testData = [{name: 'test'}, {name: 'test2'}];
-        var database = getDatabase1();
+        var database = getDatabase();
         sinon.stub(database, 'getData').withArgs('/employees').returns(testData);
         var dataStore = getEmployeeDataStore(database);
 
@@ -41,7 +36,7 @@ describe('employeeDataStore tests', function() {
 
     it('add should return the ID of the added employee from the underline data store', function(done) {
         var testData = [{name: 'test', id: 1}, {name: 'test2', id: 2}];
-        var database = getDatabase1();
+        var database = getDatabase();
         sinon.stub(database, 'getData').withArgs('/employees').returns(testData);
         var dataStore = getEmployeeDataStore(database);
 
@@ -51,7 +46,7 @@ describe('employeeDataStore tests', function() {
     });
 
     it('add should add employee to the underline data store', function(done) {
-        var database = getDatabase1();
+        var database = getDatabase();
         sinon.stub(database, 'getData').returns([]);
         sinon.stub(database, 'push');
         var dataStore = getEmployeeDataStore(database);
@@ -64,7 +59,7 @@ describe('employeeDataStore tests', function() {
     });
 
     it('add should add employee to the underline data store with an ID if no IDs exist', function(done) {
-        var database = getDatabase1();
+        var database = getDatabase();
         sinon.stub(database, 'getData').returns([]);
         sinon.stub(database, 'push');
         var dataStore = getEmployeeDataStore(database);
